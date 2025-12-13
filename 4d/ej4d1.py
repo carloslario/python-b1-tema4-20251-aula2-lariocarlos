e6c15d147f9a9b151dfa9180e8c43bd493c28a7a
carloslario <carloslariogomez@gmail.com>
2025-12-13 15:10:03 +0100

    ejercicio d subido

--- 4d/ej4d1.py
+++ 4d/ej4d1.py
@@ -16,29 +16,17 @@ Ejemplo:
     Salida:
         "Result: " 11
 
-Enunciat:
-Realitzant l'entrada per consola de les dades, implementa la funció 'sum'
-que sol·liciti l'entrada de dos números amb 'input' i torni la suma dels
-números.
-
-Paràmetre:5
-No rep cap paràmetre pel fet que dins de la funció se sol·licita la
-entrada dels números.
-
-Exemple:
-     Entrada:
-         "Insert the first number: " 8
-         "Insert the second number: " 3
-
-     Sortida:
-         "Result: " 11
 """
 
 def sum():
     # Write here your code
-    pass
+    num_1 = int(input("Insert the fierst number: "))
+    num_2 = int(input("Insert the second number "))
+    sum = num_1+   num_2
+    print(f"Result: {sum}")
+    return 
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# sum()
+
+sum()
--- 4d/ej4d3.py
+++ 4d/ej4d3.py
@@ -22,40 +22,21 @@ Ejemplo:
     Salida:
         Julio
         30
-
-Enunciat:
-
-Implementa una funció 'read_and_write', no rep cap paràmetre a causa de
-que, dins de la mateixa cal sol·licitar l'entrada de 2 dades mitjançant
-teclat.
-
-En el moment de sol·licitar l'ingrés de les dades s'ha de considerar el
-següent text.
-'Insert your name: ' El valor introduït ha de ser de tipus str.
-'Insert your age: ' El valor introduït ha de ser de tipus int.
-
-S'ha de crear un fitxer de text 'file.txt' on La informació entrada
-per consola s'ha de guardar en aquest fitxer i s'ha d'imprimir per consola
-des del fitxer de text.
-
-Paràmetre:
-No rep cap paràmetre.
-
-Exemple:
-     Entrada:
-         'Insert your name: ' Juliol
-         'Insert your age: ' 30
-     Sortida:
-         Juliol
-         30
-
 """
 
 def read_and_write():
     # Write here your code
+    name = input('Insert your name: ')
+    age = int(input('Insert your age: '))
+    with open('file.txt','w') as f_w:
+        f_w.write(f'{name}\n{age}')
+
+    with open('file.txt','r') as f_r:
+        contenido = f_r.read()
+        print(contenido)
     pass
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# read_and_write()
+
+read_and_write()
